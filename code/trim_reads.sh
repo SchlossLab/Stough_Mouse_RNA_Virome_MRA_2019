d7589ca99156e61703940fbd8ec25e9ec06d750e
JMAStough <jmastough@gmail.com>
2019-11-06 14:59:16 -0500

    fixed missing quotes

--- code/trim_reads.sh
+++ code/trim_reads.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 ####################################################################
-### This script prepares raw short reads for assembly and mapping. #
+# This script prepares raw short reads for assembly and mapping.   #
 # To run this script you must have Trimmomatic installed.          #
 ####################################################################
 
@@ -10,7 +10,7 @@
 # and creates it if it doesn't. This directory is necessary for the
 # trimming step in the code below.
 
-if [ -d "data/raw/trimmed/ ]
+if [ -d "data/raw/trimmed/" ]
 then
 	echo "Trimmed read folder already exists, continuing..."
 	echo
@@ -28,16 +28,9 @@ fi
 
 for sample in $(awk '{ print $2 }' data/process/samples.tsv); do
 
-        trimmomatic PE -threads 12 data/raw/raw/"$sample"_forward.fastq\ 
-		data/raw/raw/"$sample"_reverse.fastq\ 
-		data/raw/trimmed/"$sample"_forward_paired.fastq\ 
-		data/raw/trimmed/"$sample"_forward_unpaired.fastq\ 
-		data/raw/trimmed/"$sample"_reverse_paired.fastq\ 
-		data/raw/trimmed/"$sample"_reverse_unpaired.fastq\ 
-		LEADING:10 TRAILING:15 SLIDINGWINDOW:4:15 MINLEN:50\ 
-		ILLUMINACLIP:data/references/TruSeq3-PE.fa:2:30:10
+        trimmomatic PE -threads 12 data/raw/raw/"$sample"_forward.fastq data/raw/raw/"$sample"_reverse.fastq data/raw/trimmed/"$sample"_forward_paired.fastq data/raw/trimmed/"$sample"_forward_unpaired.fastq data/raw/trimmed/"$sample"_reverse_paired.fastq data/raw/trimmed/"$sample"_reverse_unpaired.fastq LEADING:10 TRAILING:15 SLIDINGWINDOW:4:15 MINLEN:50 ILLUMINACLIP:data/references/TruSeq3-PE.fa:2:30:10
 	cat data/raw/trimmed/"$sample"_forward_unpaired.fastq data/raw/trimmed/"$sample"_reverse_unpaired.fastq > data/raw/trimmed/"$sample"_unpaired.fastq
-	rm data/raw/trimmed/"$sample"_forward_unpaired.fastq data/raw/trimmed/"$samples_reverse_unpaired.fastq
+	rm data/raw/trimmed/"$sample"_forward_unpaired.fastq data/raw/trimmed/"$sample"_reverse_unpaired.fastq
 
 done
 
