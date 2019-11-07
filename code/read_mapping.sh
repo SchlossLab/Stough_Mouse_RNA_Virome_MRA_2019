a7312b172da51b742f83d5a3d734761331cd7fca
JMAStough <jmastough@gmail.com>
2019-11-07 13:44:04 -0500

    finished converting read mapping script

--- code/read_mapping.sh
+++ code/read_mapping.sh
@@ -1,75 +1,28 @@
 #!/usr/bin/bash
 
 ###########################################################################
-# This script processes assembled scaffolds, adding treatment groups to   #
-# fasta sequence headers, removing short contigs, removing line breaks    #
-# from fasta sequences, checks for circular contigs, and maps trimmed     #
-# RNAseq short reads to scaffolds for quantification of relative          #
-# abundance.                                                              #
-# Working installations of Bowtie2, BBMap, and CContigs is required to    #
+# This script processes maps trimmed short reads to scaffolds for         # 
+# quantification of relative abundance.                                   #
+# A working installation of Bowtie2 is required to                        #
 # to run this script.                                                     #
 ###########################################################################
 
-### This chunk 
+### This chunk uses the build function from Bowtie 2 to make index files
+# from the whole scaffold file for mapping short reads to scaffolds.
 
-if [ -e $scaffold_path/all_raw_scaffolds.fasta ]
-then
-    	echo "Deleting old temp contig file"
-        rm $scaffold_path/all_raw_scaffolds.fasta
-else
-    	echo "No old contig files to delete"
-fi
+bowtie2-build data/raw/scaffolds/all_scaffolds.fasta data/mapping/index/all_scaffolds 
 
-echo
+### This chunck runs read mappings, converts SAM files to BAM format, 
+# and sorts the BAM files.
 
-### Add filenames to fasta headers and concatenate contigs into single file
+for $treatment in $(awk '{ print $2 }' data/process/samples.txt); do
 
-echo "Adding filenames to fasta headers and concatenating contigs into single file"
-echo
-
-for sample in $(ls data/process/metatrans_contigs/); do
-
-        cp data/process/metatrans_contigs/"$sample"/transcripts.fasta "$scaffold_path"/"$sample"_scaffolds.fasta
-        sed 's/^>/>'"$sample"'_/g' "$scaffold_path"/"$sample"_scaffolds.fasta >> "$scaffold_path"/all_raw_scaffolds.fasta
-
-done
-
-
-### Remove contigs under 1000 bases
-
-echo "Removing contigs under 1000 bases"
-echo
-
-reformat.sh minlength=1000 overwrite=t in="$scaffold_path"/all_raw_scaffolds.fasta out="$scaffold_path"/all_long_scaffolds.fasta
-
-
-### Create temporary contig file and remove line breaks from the sequences
-
-echo "Creating temporary contig file and removing line breaks from the sequences"
-echo
-awk '!/^>/ { printf "%s", $0; n = "\n" } /^>/ { print n $0; n = "" } END { printf "%s", n }' "$scaffold_path"/all_long_scaffolds.fasta > "$scaffold_path"/all_scaffolds.fasta
-
-### Search for Circular Contigs
-
-julia ccontigs.jl -i "$scaffold_path"/all_scaffolds.fasta -o "$cir_contig_path"/ccontigs_vlp_out.tsv
-
-### Build Bowtie index files
-
-bowtie2-build "$scaffold_path"/all_scaffolds.fasta data/references/metatrans_index/all_scaffolds 
-
-### Run Read mappings, convert to BAM format, and sort
-
-for line in $(cat data/process/trimmed_metatrans/samples.txt); do
-
-	bowtie2 -x data/references/metatrans_index/all_scaffolds -1 data/process/trimmed_metatrans/"$line"_forward_paired.fastq\
-	-2 data/process/trimmed_metatrans/"$line"_reverse_paired.fastq -U data/process/trimmed_metatrans/"$line"_unpaired.fastq\
-	-S data/process/mapping/metatrans/"$line".sam --end-to-end --sensitive -p 12 -t > data/process/mapping/metatrans/"$line"_mapping_stats.txt
-
-	samtools view -S -b data/process/mapping/metatrans/"$line".sam > data/process/mapping/metatrans/"$line".bam
-
-	samtools sort data/process/mapping/metatrans/"$line".bam -o data/process/mapping/metatrans/"$line"_sorted.bam
-
-	samtools idxstats data/process/mapping/metatrans/"$line"_sorted.bam > data/process/mapping/metatrans/"$line".tsv
+	bowtie2 -x data/raw/mapping/index/all_scaffolds -1 data/raw/trimmed/"$treatment"_forward_paired.fastq\
+	-2 data/raw/trimmed/"$treatment"_reverse_paired.fastq -U data/raw/trimmed/"$treatment"_unpaired.fastq\
+	-S data/raw/mapping/"$treatmenr".sam --end-to-end --sensitive -p 12 -t
+	samtools view -S -b data/raw/mapping/"$treatment".sam > data/raw/mapping/"$treatment".bam
+	samtools sort data/raw/mapping/"$treatment".bam -o data/raw/mapping/"$treatment"_sorted.bam
+	rm data/raw/mapping/"$treatment".sam
 
 done
 
