0aac611e10aa189db1921ca93edb962cd70d6263
JMAStough <jmastough@gmail.com>
2019-12-06 12:51:05 -0500

    fixed finish trees script

--- code/finish_trees.R
+++ code/finish_trees.R
@@ -5,4 +5,4 @@ source("code/edit_mito_tree.R")
 figure1 <- plot_grid(astro_tree_figure, mito_tree_figure, 
                      labels = c("A", "B"))
 
-ggsave("results/figures/Figure1.png", figure1, width = 10, height = 8)
+ggsave("results/figures/Figure1.png", figure1, width = 12, height = 8)
