3c409696fbf2df935cd937707b67520b34b00d68
JMAStough <jmastough@gmail.com>
2019-12-06 13:50:10 -0500

    finished Figure 1

--- code/finish_trees.R
+++ code/finish_trees.R
@@ -3,6 +3,6 @@ source("code/edit_astro_tree.R")
 source("code/edit_mito_tree.R")
 
 figure1 <- plot_grid(astro_tree_figure, mito_tree_figure, 
-                     labels = c("A", "B"))
+                     labels = c("A", "B"), label_size = 30)
 
 ggsave("results/figures/Figure1.png", figure1, width = 12, height = 8)
